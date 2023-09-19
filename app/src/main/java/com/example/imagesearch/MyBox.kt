7f6959cf86fed74dfcb353d66890efad9fd314bd
Martin Kwon <mindori9097@gmail.com>
2023-09-19 17:54:42 +0900

    [UI] item_image.xml 아이템 추가

--- app/src/main/java/com/example/imagesearch/MyBox.kt
+++ app/src/main/java/com/example/imagesearch/MyBox.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.imagesearch.databinding.FragmentMyBoxBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +18,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MyBox : Fragment() {
+
+    private var _binding: FragmentMyBoxBinding? = null
+    private val binding get() = _binding!!
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
--- app/src/main/java/com/example/imagesearch/Search.kt
+++ app/src/main/java/com/example/imagesearch/Search.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.imagesearch.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +18,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Search : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
