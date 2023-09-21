f9055dd6a3e0f693309e27f87471e0407a503d17
Martin Kwon <mindori9097@gmail.com>
2023-09-22 01:41:49 +0900

    [Docs] modified

--- app/src/main/java/com/example/imagesearch/data/Constant.kt
+++ app/src/main/java/com/example/imagesearch/data/Constant.kt
@@ -6,7 +6,7 @@ class Constant {
         const val AUTH_HEADER = "KakaoAK e99b1394104cb0bece454668842b2158"
 //        const val REST_API_KEY = "e99b1394104cb0bece454668842b2158"
 //        const val AUTH_HEADER = "KakaoAK $REST_API_KEY"
-        const val PREFS_NAME = "com.martinK.imagesearch.prefs"
-        const val PREF_KEY = "IMAGE_SEARCH_PREF"
+        const val PREFS_NAME = "com.example.imagesearch.prefs"
+        const val PREF_KEY = "img"
     }
 }
