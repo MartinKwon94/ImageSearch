a867006bd4d5e972da9f7af764c97bb3f4efd71a
Martin Kwon <mindori9097@gmail.com>
2023-09-22 01:15:44 +0900

    [Docs] 둘이 무슨 차이?

--- app/src/main/java/com/example/imagesearch/data/Constant.kt
+++ app/src/main/java/com/example/imagesearch/data/Constant.kt
@@ -4,6 +4,8 @@ class Constant {
     companion object {
         const val BASE_URL = "https://dapi.kakao.com"
         const val AUTH_HEADER = "KakaoAK e99b1394104cb0bece454668842b2158"
+//        const val REST_API_KEY = "e99b1394104cb0bece454668842b2158"
+//        const val AUTH_HEADER = "KakaoAK $REST_API_KEY"
         const val PREFS_NAME = "pref"
         const val PREF_KEY = "img"
     }
