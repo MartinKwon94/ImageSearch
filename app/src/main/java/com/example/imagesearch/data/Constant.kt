535ad98a7a08b538e18b9af120a71dd01d055e1f
Martin Kwon <mindori9097@gmail.com>
2023-09-22 09:17:17 +0900

    [Docs] minor modified

--- app/src/main/java/com/example/imagesearch/data/Constant.kt
+++ app/src/main/java/com/example/imagesearch/data/Constant.kt
@@ -4,9 +4,6 @@ object Constant {
 
     const val BASE_URL = "https://dapi.kakao.com"
     const val AUTH_HEADER = "KakaoAK e99b1394104cb0bece454668842b2158"
-
-    //        const val REST_API_KEY = "e99b1394104cb0bece454668842b2158"
-//        const val AUTH_HEADER = "KakaoAK $REST_API_KEY"
     const val PREFS_NAME = "com.example.imagesearch.prefs"
     const val PREF_KEY = "img"
 }
